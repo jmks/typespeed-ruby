89a3752ade556dd80c203b0d4b3ca91c168945ac
jmks <jason.schweier@gmail.com>
2016-01-31 16:48:30 -0500

    Added UserInput to manage user submitted text

--- game.rb
+++ game.rb
@@ -5,6 +5,7 @@ require_relative "lib/states"
 require_relative "lib/delta_time"
 
 require_relative "lib/word"
+require_relative "lib/user_input"
 require_relative "lib/typespeed"
 
 Typespeed.new.show
---
+++ lib/user_input.rb
@@ -0,0 +1,20 @@
+class UserInput
+  attr_reader :game, :last_submission
+
+  def initialize(game)
+    @game = game
+    game.text_input = @input = build_input
+    @last_submission = ""
+  end
+
+  def submit!
+    @last_submission = @input.text
+    game.text_input  = @input = build_input
+  end
+
+  private
+
+  def build_input
+    Gosu::TextInput.new
+  end
+end
