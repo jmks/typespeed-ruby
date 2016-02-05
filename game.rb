8c8e09da09dc975e0530c2169288e8f77f8ba267
jmks <jason.schweier@gmail.com>
2016-02-04 19:32:56 -0500

    Moved Gosu framework code to lib/framework

--- game.rb
+++ game.rb
@@ -1,8 +1,8 @@
 require "gosu"
 require "pry"
 
-require_relative "lib/states"
-require_relative "lib/delta_time"
+require_relative "lib/framework/states"
+require_relative "lib/framework/delta_time"
 
 require_relative "lib/word"
 require_relative "lib/word_generator"
